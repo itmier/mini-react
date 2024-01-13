7e74df45de0df9fe7d1825d2dc7ecfe13e676afe
Tmier <itmier@126.com>
2024-01-13 23:04:26 +0800

    feat: 使用vite实现jsx写法支持

---
+++ V02/vite-runner/App.jsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: Tmier
+ * @Date: 2024-01-13 22:44:52
+ * @LastEditTime: 2024-01-13 22:47:49
+ * @LastEditors: Tmier
+ * @Description: 
+ * 
+ */
+import React from './core/React.js'
+// const App = React.createElement('div', { id: 'app' }, ' Hello World', 'Hello', 'Mini-React')
+const App =<div id='app'>
+  Hello World
+  <h2>Hello MiniReact</h2>
+</div>
+
+export default App
