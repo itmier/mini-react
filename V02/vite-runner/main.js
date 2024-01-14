2164919c325cc3bb554aabeeb75b1831462f59a8
Tmier <itmier@126.com>
2024-01-14 21:59:35 +0800

    feat: requestIdleCallback

--- V02/vite-runner/main.js
+++ V02/vite-runner/main.js
@@ -1,4 +1,10 @@
 import ReactDom from './core/ReactDom.js'
 import App from './App.jsx'
 
+function WorkLoop(deadline) {
+  console.log(deadline.timeRemaining());
+  // requestIdleCallback(WorkLoop)
+}
+requestIdleCallback(WorkLoop)
+
 ReactDom.createRoot(document.querySelector('#root')).render(App)
